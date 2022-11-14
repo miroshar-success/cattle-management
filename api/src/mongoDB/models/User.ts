f6a9655470cfc6e20ae2c873d8a2dbf7bd376cbd
oskarmast <oskarmast9436@gmail.com>
2022-11-14 14:09:16 -0300

    mdb/Crear models

---
+++ api/src/mongoDB/models/User.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+import { animalSchema, IAnimal } from "./Animal";
+import { INote, noteSchema } from "./Note";
+
+export interface IUser {
+  _id: string; //! Esto podría ser que me de problemas al momento de asignarle el id al usuario nuevo, que va a ser un string según el req.auth.sub
+  name?: string | undefined;
+  email: string;
+  profile_img?: string;
+  animals: [IAnimal];
+  notes: [INote];
+}
+
+// export interface IUserModel extends IUser {}
+
+export const userSchema: Schema = new Schema({
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, lowercase: true },
+  profile_img: { type: String, required: false },
+  animals: [animalSchema],
+  notes: [noteSchema],
+});
+
+// export const User = mongoose.model<IUserModel>("User", userSchema);
