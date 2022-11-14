a4f9d2a16a5e7632e8468e84db907768b0f8d0cf
oskarmast <oskarmast9436@gmail.com>
2022-11-13 22:56:26 -0300

    pro/Agregar timestamps a animalSchema

--- api/src/mongoDB/setup.js
+++ api/src/mongoDB/setup.js
@@ -8,30 +8,36 @@ mongoose.connect(MONGO_URI, {
 
 const Schema = mongoose.Schema;
 
-const noteSchema = new Schema({
-  // _id: String,
-  title: String,
-  theme: String,
-  comment: String,
-  importance: String,
-});
+const noteSchema = new Schema(
+  {
+    // _id: String,
+    title: String,
+    theme: String,
+    comment: String,
+    importance: String,
+  },
+  { timestamps: true }
+);
 
-const animalSchema = new Schema({
-  _id: { type: String, required: true },
-  type_of_animal: String,
-  breed_name: String,
-  location: String,
-  weight_kg: Number,
-  name: { type: String, lowercase: true },
-  device_type: String,
-  device_number: String,
-  comments: String,
-  images: [String],
-  birthday: { type: Date, required: false },
-  is_pregnant: Boolean,
-  delivery_date: { type: Date, required: false },
-  UserId: { type: String, required: true },
-});
+const animalSchema = new Schema(
+  {
+    _id: { type: String, required: true },
+    type_of_animal: String,
+    breed_name: String,
+    location: String,
+    weight_kg: Number,
+    name: { type: String, lowercase: true },
+    device_type: String,
+    device_number: String,
+    comments: String,
+    images: [String],
+    birthday: { type: Date, required: false },
+    is_pregnant: Boolean,
+    delivery_date: { type: Date, required: false },
+    UserId: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const userSchema = new Schema({
   _id: { type: String, required: true },
@@ -45,33 +51,3 @@ const userSchema = new Schema({
 export const User = mongoose.model("User", userSchema);
 export const Note = mongoose.model("Note", noteSchema);
 export const Animal = mongoose.model("Animal", animalSchema);
-
-// const createAndSavePerson = (done) => {
-//   let eltonJohn = new Person({
-//     name: "Elton John",
-//     age: 32,
-//     favoriteFoods: ["Arroz", "Pollo"],
-//   });
-//   eltonJohn.save(function (error, data) {
-//     if (error) {
-//       return console.log(error);
-//     }
-//     done(null, data);
-//   });
-// };
-
-// var arrayOfPeople = [
-//   { name: "Frankie", age: 74, favoriteFoods: ["Del Taco"] },
-//   { name: "Sol", age: 76, favoriteFoods: ["roast chicken"] },
-//   { name: "Robert", age: 78, favoriteFoods: ["wine"] },
-// ];
-
-// const createManyPeople = (arrayOfPeople, done) => {
-//   Person.create(arrayOfPeople, function (error, data) {
-//     if (error) {
-//       return console.log(error);
-//     } else {
-//       done(null, data);
-//     }
-//   });
-// };
