1d24c52167ae9ab1bf2934d078f3f522f6a8dedc
oskarmast <oskarmast9436@gmail.com>
2022-11-15 11:28:34 -0300

    noIMP/Agregar comentario

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ const config = require(__dirname + "/src/config/config.js");
 const PORT = config.server.port || 3001;
 const app = require("./src/app");
 
+// Primero sincroniza con la database, y luego levanta el servidor :
 db.sequelize.sync({ alter: true }).then(() => {
   app.listen(PORT, () => {
     console.log(
