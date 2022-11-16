347dd5cd88c9e1a85da9e8fdefee53d6574e1366
oskarmast <oskarmast9436@gmail.com>
2022-11-16 15:49:01 -0300

    refactor/Eliminar sequelize.sync y dejar únicamente app.listen()

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,13 @@
-import db from "./src/models";
 const config = require(__dirname + "/src/config/config.js");
 const PORT = config.server.port || 3001;
 const app = require("./src/app");
 
-// Primero sincroniza con la database, y luego levanta el servidor :
-db.sequelize.sync({ alter: true }).then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      "**** **** **** **** **** **** **** **** **** **** **** **** **** **** **** "
-    );
-    console.log(`App listening on port ${PORT}`);
-    console.log(
-      "**** **** **** **** **** **** **** **** **** **** **** **** **** **** **** "
-    );
-  });
+app.listen(PORT, () => {
+  console.log(
+    "**** **** **** **** **** **** **** **** **** **** **** **** **** **** **** "
+  );
+  console.log(`App listening on port ${PORT}`);
+  console.log(
+    "**** **** **** **** **** **** **** **** **** **** **** **** **** **** **** "
+  );
 });
