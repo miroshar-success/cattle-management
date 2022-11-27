5c1a6896908ffaf2c078baa9915b51553ccecd78
oskarmast <oskarmast9436@gmail.com>
2022-11-27 20:13:18 -0300

    refactor/Modificar .env y MONGO_URI en vez de username y password

--- api/src/config/configMDB.ts
+++ api/src/config/configMDB.ts
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@mycattlelogv1mdb.xc08ugu.mongodb.net/?retryWrites=true&w=majority`;
+// const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+// const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+// const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@mycattlelogv1mdb.xc08ugu.mongodb.net/?retryWrites=true&w=majority`;
+const MONGO_URI = process.env.MONGO_URI;
 
 const SERVER_PORT = process.env.SERVER_PORT
   ? Number(process.env.SERVER_PORT)
@@ -11,7 +12,7 @@ const SERVER_PORT = process.env.SERVER_PORT
 
 export const config = {
   mongo: {
-    url: MONGO_URL,
+    url: MONGO_URI,
   },
   server: {
     port: SERVER_PORT,
