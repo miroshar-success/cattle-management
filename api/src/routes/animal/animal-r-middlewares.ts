7153d6739157b7b4c90f8227a51b52fbb7e24baa
oskarmast <oskarmast9436@gmail.com>
2022-11-27 16:09:13 -0300

    refactor/GRAN MODULARIZACIÓN de las rutas

---
+++ api/src/routes/animal/animal-r-middlewares.ts
@@ -0,0 +1,254 @@
+import { Response, Request } from "express";
+import { Request as JWTRequest } from "express-jwt";
+
+import { checkAnimal } from "../../validators/animal-validators";
+import { getUserByIdOrThrowError } from "../user/user-r-auxiliary";
+import {
+  getPregnantInDeliveryDateOrder,
+  typesOfAnimalsToArray,
+} from "./animal-r-auxiliary";
+import { getStatsObjEvo } from "./getStatsAuxFn";
+import { Animal } from "../../mongoDB";
+import { IAnimal } from "../../mongoDB/models/Animal";
+
+//* MIDDLEWARE FUNCTION FOR THE REQUESTS :
+
+// GET STATS :
+export async function handleGetStatsRequest(req: JWTRequest, res: Response) {
+  try {
+    const user_id = req.auth?.sub;
+    if (!user_id) {
+      throw new Error("User id no puede ser falso.");
+    }
+    const stats = await getStatsObjEvo(user_id);
+
+    console.log(`Devolviendo objeto stats...`);
+    return res.status(200).send(stats);
+  } catch (error: any) {
+    console.log(`Error en GET 'animal/stats'. ${error.message}`);
+    return res.status(400).send({ error: error.message });
+  }
+}
+
+// GET ALL ANIMALS FROM USER :
+export async function handleGetAllAnimalsFromUserRequest(
+  req: JWTRequest,
+  res: Response
+) {
+  try {
+    const user_id = req.auth?.sub;
+    if (!user_id) {
+      throw new Error("User id no puede ser falso.");
+    }
+    const userInDB = await getUserByIdOrThrowError(user_id);
+    let animalsFromUser: IAnimal[] = userInDB.animals;
+    return res.status(200).send(animalsFromUser);
+  } catch (error: any) {
+    console.log(`Error en "/animal/". ${error.message}`);
+    return res.send({ error: error.message });
+  }
+}
+
+// POST NEW ANIMAL:
+export async function handlePostNewAnimalRequest(
+  req: JWTRequest,
+  res: Response
+) {
+  try {
+    const user_id = req.auth?.sub;
+    if (!user_id) {
+      throw new Error("User id no puede ser falso.");
+    }
+    const userInDB = await getUserByIdOrThrowError(user_id);
+
+    console.log(`REQ.BODY = `);
+    console.log(req.body);
+    const validatedNewAnimal = checkAnimal({ ...req.body, user_id });
+    const newAnimalCreated = await Animal.create(validatedNewAnimal);
+    userInDB?.animals.push(newAnimalCreated);
+    userInDB.animalsPop.push(newAnimalCreated._id);
+    await userInDB.save();
+    console.log(`nuevo animal creado y pusheado al usuario con id ${user_id}`);
+    return res.status(200).send({
+      msg: "Animal creado correctamente.",
+      animal: newAnimalCreated,
+    });
+  } catch (error: any) {
+    console.log(`Error en POST 'user/'. ${error.message}`);
+    return res.status(400).send({ error: error.message });
+  }
+}
+
+// DELETE ANIMAL :
+export async function handleDeleteAnimalById(req: JWTRequest, res: Response) {
+  try {
+    const user_id = req.auth?.sub;
+    const id_senasaFromParams: string = req.params.id_senasa;
+
+    if (!id_senasaFromParams) {
+      throw new Error(`El id de SENASA es falso.`);
+    }
+    if (!user_id) {
+      throw new Error("El user id es falso.");
+    }
+    let userInDB = await getUserByIdOrThrowError(user_id);
+    if (userInDB === null) {
+      throw new Error(
+        `Usuario con id '${user_id}'no encontrado en la base de datos.`
+      );
+    }
+
+    await userInDB.animals.id(id_senasaFromParams)?.remove();
+    await userInDB.save();
+
+    return res
+      .status(200)
+      .send({ msg: "Animal eliminado exitosamente", status: true });
+  } catch (error: any) {
+    console.log(`Error en DELETE por id. ${error.message}`);
+    return res.status(400).send({ error: error.message });
+  }
+}
+
+// GET BY ID_SENASA :
+export async function handleGetAnimalByIdRequest(
+  req: JWTRequest,
+  res: Response
+) {
+  try {
+    const { id_senasa } = req.params;
+
+    const user_id = req.auth?.sub;
+    if (!user_id) {
+      throw new Error("El user id es falso.");
+    }
+    const userInDB = await getUserByIdOrThrowError(user_id);
+    let foundAnimal = userInDB.animals.id(id_senasa);
+
+    if (foundAnimal) {
+      return res.status(200).send(foundAnimal);
+    } else {
+      return res.status(404).send({
+        error: `No se encontró ningún registro con el id '${id_senasa}'.`,
+      });
+    }
+  } catch (error: any) {
+    console.log(`Error en GET "/:id_senasa". ${error.message}`);
+    return res.status(400).send({ error: error.message });
+  }
+}
+
+// UPDATE ANIMAL :
+export async function handleUpdateAnimalRequest(
+  req: JWTRequest,
+  res: Response
+) {
+  try {
+    console.log(`REQ.BODY = `);
+    console.log(req.body);
+
+    const userId = req.auth?.sub;
+    if (!userId) {
+      throw new Error("El user id es falso");
+    }
+    const userInDB = await getUserByIdOrThrowError(userId);
+    const validatedAnimal: IAnimal = checkAnimal({ ...req.body, userId });
+    let foundAnimal = userInDB.animals.id(validatedAnimal._id);
+    foundAnimal.type_of_animal = validatedAnimal.type_of_animal;
+    foundAnimal.breed_name = validatedAnimal.breed_name;
+    foundAnimal.location = validatedAnimal.location;
+    foundAnimal.weight_kg = validatedAnimal.weight_kg;
+    foundAnimal.name = validatedAnimal.name;
+    foundAnimal.device_type = validatedAnimal.device_type;
+    foundAnimal.device_number = validatedAnimal.device_number;
+    foundAnimal.images = validatedAnimal.images;
+    foundAnimal.comments = validatedAnimal.comments;
+    foundAnimal.birthday = validatedAnimal.birthday;
+    foundAnimal.is_pregnant = validatedAnimal.is_pregnant;
+    foundAnimal.delivery_date = validatedAnimal.delivery_date;
+    foundAnimal.sex = validatedAnimal.sex;
+
+    await userInDB.save();
+
+    console.log(`Animal actualizado. Retornando respuesta...`);
+
+    return res.send({
+      updated: Number(1),
+      msg: `Cantidad de animales actualizados correctamente: ${1}`,
+    });
+  } catch (error: any) {
+    console.log(`Error en PUT "/animal". ${error.message}`);
+    return res.status(400).send({ error: error.message });
+  }
+}
+
+// GET TYPES OF ANIMAL ACCEPTED :
+export async function handleGetTypesAllowedRequest(
+  req: JWTRequest,
+  res: Response
+) {
+  try {
+    let typesOfAnimalsArray = typesOfAnimalsToArray();
+    return res.status(200).send(typesOfAnimalsArray);
+  } catch (error: any) {
+    console.log(`Error en GET 'animal/typesAllowed. ${error.message}`);
+    return res.status(400).send({ error: error.message });
+  }
+}
+
+// SEARCH BY QUERY :
+export async function handleSearchByQueryRequest(
+  req: JWTRequest,
+  res: Response
+) {
+  try {
+    let queryValue = req.query.value;
+
+    const user_id = req.auth?.sub;
+    if (!user_id) {
+      throw new Error("El user id es falso.");
+    }
+    if (typeof queryValue === "string") {
+      queryValue = queryValue.toLowerCase();
+    }
+    // buscar coincidencias adentro de User con forEach :
+    const userInDB = await getUserByIdOrThrowError(user_id);
+    if (userInDB === null) {
+      throw new Error("Usuario no encontrado");
+    }
+
+    const userAnimals: IAnimal[] = userInDB.animals;
+    let animalesMatched: IAnimal[] = [];
+    userAnimals.forEach((element: IAnimal) => {
+      if (
+        element?.id_senasa?.toLowerCase() === queryValue ||
+        element?.name?.toLowerCase() === queryValue ||
+        element?.device_number?.toLowerCase() === queryValue
+      ) {
+        animalesMatched.push(element);
+      }
+    });
+
+    return res.status(200).send(animalesMatched);
+  } catch (error: any) {
+    return res.status(400).send({ error: error.message });
+  }
+}
+
+// GET PREGNANT ORDERED BY DELIVERY DATE :
+export async function handleGetPregnantSortedRequest(
+  req: Request,
+  res: Response
+) {
+  try {
+    const arrayOfPregnantOrderedByDeliveryDate =
+      await getPregnantInDeliveryDateOrder(
+        "google-oauth2|112862055400782384259"
+      );
+    console.log(`Retornando arreglo....`);
+    return res.status(200).send(arrayOfPregnantOrderedByDeliveryDate);
+  } catch (error: any) {
+    console.log(`Error en "/pregnant". ${error.message}`);
+    return res.status(400).send({ error: error.message });
+  }
+}
--- api/src/routes/user/user-routes.ts
+++ api/src/routes/user/user-routes.ts
@@ -1,88 +1,17 @@
-import { IReqAuth } from "../../types/user-types";
-import { validateNewUser } from "../../validators/user-validators";
-import jwtCheck from "../../config/jwtMiddleware";
 import { Router } from "express";
-import { emailExistsInDataBase } from "./user-r-auxiliary";
+import jwtCheck from "../../config/jwtMiddleware";
 
 const router = Router();
 
-// -------- MONGO / MONGOOSE : ------------
-import { User } from "../../mongoDB/";
-import { INewUser, IUser } from "../../mongoDB/models/User";
+import {
+  handleDoesUserExistInDB,
+  handleGetUserInfo,
+  handleRegisterNewUser,
+} from "./user-r-middlewares";
 
-// GET ALL USERS :
-// router.get("/", async (req, res) => {
-//   try {
-//     const users: IUser[] = await User.find().lean();
-//     return res.status(200).send(users);
-//   } catch (error: any) {
-//     console.log(`Error en ruta GET "user/". ${error.message}`);
-//     return res.status(400).send({ error: error.message });
-//   }
-// });
-
-// POST NEW USER
-router.post("/register", jwtCheck, async (req: any, res) => {
-  try {
-    console.log(`REQ.BODY =`);
-    console.log(req.body);
-    const reqAuth: IReqAuth = req.auth;
-    const _id = reqAuth.sub;
-    const { name, email, profile_img } = req.body;
-    await emailExistsInDataBase(email);
-    const validatedUser: INewUser = validateNewUser(
-      _id,
-      name,
-      email,
-      profile_img
-    );
-
-    const newUser = await User.create(validatedUser);
-    console.log(newUser);
-    return res.status(201).send(newUser);
-  } catch (error: any) {
-    console.log(`Error en ruta POST "user/". ${error.message}`);
-    return res.status(400).send({ error: error.message });
-  }
-});
-
-// USER EXISTS IN THE DATA BASE
-router.get("/existsInDB", jwtCheck, async (req: any, res) => {
-  try {
-    const reqAuth: IReqAuth = req.auth;
-    const userId = reqAuth.sub;
-    // const isUserRegisteredinDB = await userIsRegisteredInDB(userId);
-    const isUserRegisteredinDB = await User.findById(userId);
-    if (isUserRegisteredinDB) {
-      return res.status(200).send({ msg: true });
-    }
-    if (!isUserRegisteredinDB) {
-      console.log(`Usuario no encontrado en la DB.`);
-      return res.status(200).send({ msg: false });
-    }
-  } catch (error: any) {
-    console.log(`Error en GET "/user/existsInDB. ${error.message}`);
-    return res.status(400).send({ error: error.message });
-  }
-});
-
-// USER INFO :
-router.get("/userInfo", jwtCheck, async (req: any, res) => {
-  try {
-    const reqAuth: IReqAuth = req.auth;
-    const userId: string = reqAuth.sub;
-    // await throwErrorIfUserIsNotRegisteredInDB(userId);
-    const userInfo = await User.findById(userId).populate("animalsPop");
-    if (!userInfo) {
-      throw new Error(
-        `El usuario con id '${userId}'no fue encontrado en la Data Base`
-      );
-    }
-    return res.status(200).send(userInfo);
-  } catch (error: any) {
-    console.log(`Error en 'user/userInfo'. ${error.message}`);
-    return res.status(400).send({ error: error.message });
-  }
-});
+router.post("/register", jwtCheck, handleRegisterNewUser);
+router.get("/existsInDB", jwtCheck, handleDoesUserExistInDB);
+router.get("/userInfo", handleGetUserInfo);
+// router.get("/", getAllUsersInDB);
 
 export default router;
