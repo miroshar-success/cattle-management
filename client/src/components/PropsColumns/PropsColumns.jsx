5ee71b110370681a6bd666ec08f9893093a87a1d
oskarmast <oskarmast9436@gmail.com>
2022-11-28 17:25:21 -0300

    feat/Agregar edad del animal

--- client/src/components/PropsColumns/PropsColumns.jsx
+++ client/src/components/PropsColumns/PropsColumns.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { AnimalCard } from "../AnimalCard/AnimalCard";
 import { Pagination } from "../Pagination/Pagination";
-import { parseDate } from "../../constants/parseDateFn";
+import { parseDate, calculateAge } from "../../constants/parseDateFn";
 export function PropsColumns({ animals }) {
   const [page, setPage] = useState(1);
   const showPerPage = 8;
@@ -28,6 +28,9 @@ export function PropsColumns({ animals }) {
 
                 <th className="px-5 py-3 w-fit border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                   Peso (kg)
+                </th>
+                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Edad (años)
                 </th>
                 <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                   Nombre
@@ -66,6 +69,11 @@ export function PropsColumns({ animals }) {
                   <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                     <p className="text-gray-900 whitespace-no-wrap capitalize">
                       {animal?.weight_kg}
+                    </p>
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap capitalize">
+                      {calculateAge(animal?.birthday)}
                     </p>
                   </td>
                   <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
