a7dd233ded52dded03f93a6f32015c14784159a9
oskarmast <oskarmast9436@gmail.com>
2022-12-05 11:29:25 -0300

    tsc build

--- api/build/src/config/configMDB.js
+++ api/build/src/config/configMDB.js
@@ -6,9 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = exports.MONGO_URI = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-// const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
-// const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
-// const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@mycattlelogv1mdb.xc08ugu.mongodb.net/?retryWrites=true&w=majority`;
 exports.MONGO_URI = process.env.MONGO_URI;
 const SERVER_PORT = process.env.SERVER_PORT
     ? Number(process.env.SERVER_PORT)
@@ -21,4 +18,3 @@ exports.config = {
         port: SERVER_PORT,
     },
 };
-// module.exports = config;
