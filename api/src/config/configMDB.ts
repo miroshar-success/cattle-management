1436d647b2320db1f8efcbc5dbd22ebfd98ff313
oskarmast <oskarmast9436@gmail.com>
2022-12-05 11:27:46 -0300

    borrar comments

--- api/src/config/configMDB.ts
+++ api/src/config/configMDB.ts
@@ -1,9 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
-// const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
-// const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@mycattlelogv1mdb.xc08ugu.mongodb.net/?retryWrites=true&w=majority`;
 export const MONGO_URI = process.env.MONGO_URI;
 
 const SERVER_PORT = process.env.SERVER_PORT
